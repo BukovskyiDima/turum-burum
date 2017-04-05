83d192b6a4442b4731b41df3f75bf426f09413c2
Dima Bukovskyi <buovsky.dima@gmail.com>
2017-04-06 02:14:55 +0300

    добавление функциональности, добавление коментариев

--- js/main.js
+++ js/main.js
@@ -3,21 +3,46 @@ document.addEventListener('DOMContentLoaded', function() {
         showListMain = doc.querySelector('.list__main-show'),
         showListSecond = doc.querySelector('.list__main');
 
-    let showMainList = function() {
+    // показать первый список
+    let showMainList = function () {
         let showBlock = document.querySelector('.list__main');
 
         showBlock.classList.toggle('hide');
     }
 
-    showListMain.addEventListener('click' ,showMainList );
+    showListMain.addEventListener('click', showMainList);
 
-    let showSecondList = function(e) {
-        if(e.target.tagName === 'LI'){
-            e.target.classList.toggle('show');
-        } else if (e.target.parentNode.tagName == 'LI') {
-            e.target.parentNode.classList.toggle('show');
+    //поднять элемент на верх
+    let goUp = function (e) {
+        let firstElem = doc.querySelector('.list__main-show'),
+            isLi = e.target.tagName === 'LI';
+
+        if (e.target.tagName === isLi) {
+            firstElem.innerHTML = e.target.dataset.name + ':';
+            firstElem.dataset.name = e.target.dataset.name;
+        } else if (e.target.closest('.list__main > li')) {
+            firstElem.innerHTML = e.target.closest('.list__main > li').dataset.name + ':';
+            firstElem.dataset.name = e.target.closest('.list__main > li').dataset.name;
+        } else {
+            firstElem.innerHTML = firstElem.dataset.name + ':' || firstElem.closest('.list__main > li').dataset.name;
         }
     }
 
-    showListSecond.addEventListener('click' ,showSecondList );
+    showListSecond.addEventListener('click', goUp);
+
+    //показать второй списек
+
+    let showSecondList = function (e) {
+        let isLi = e.target.tagName === 'LI';
+
+        if (e.target.tagName === isLi) {
+            e.target.classList.toggle('show');
+        } else if (e.target.closest('.list__main-with_some') || e.target.closest('.list__main-without_some')) {
+            e.target.closest('li').classList.toggle('show');
+        }
+    }
+
+    showListSecond.addEventListener('click', showSecondList);
+
+    //интерактивный поиск
 });
